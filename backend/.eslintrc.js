sanialecs/centrale-ei-web

:tada: initial commit

backend/.eslintrc.js
@@ -1,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "prettier/prettier": "warn",
+    "no-unreachable": "warn",
+  },
+};

ElfieMoBo/in211

backend/.eslintrc.js
@@ -1,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-unreachable": "error",
+  },
+};