f41a0a62675a0f0ca951e62f4ec6fa1907b49d91
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-04-01 12:13:13 +0200

    tp1 - recherche api

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './Home.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [movieSearch, setSearch] = useState('coucou')
+  const [moviesList, setlist] = useState([])
+  useEffect( () => {
+    axios
+      .get('https://api.themoviedb.org/3/trending/movie/week?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb')
+      .then((reponse) => {
+        setlist(reponse.data.results)
+        console.log(reponse)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello !</h1>
+        <header className="App-header">
+          <input
+            type="text"
+            id="film_name"
+            name="filmname"  
+            placeholder="Rechercher un film"
+            onChange={function(event){setSearch(event.target.value)}}
+            // Entre {} -> js
+          >
+          </input>
+          <p>
+            {movieSearch}
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/Home/Home.jsx</code> and save to reload.
+          </p>
+          <p>
+            Liste des films : 
+            {moviesList.map(title => <li>{moviesList[6].original_title}</li>)}
+          </p>
+        </header>
     </div>
   );
 }
