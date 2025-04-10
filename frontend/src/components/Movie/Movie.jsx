5b7e7cccadbdc494abee2d5281fd62bb37fb0013
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-04-10 08:30:17 +0200

    (feature) navigating to a details page when clicking on a film

--- frontend/src/components/Movie/Movie.jsx
+++ frontend/src/components/Movie/Movie.jsx
@@ -1,11 +1,14 @@
-
+import { useNavigate } from 'react-router-dom';
 import './Movie.css';
 
 function Movie({ movie }) {
+    const navigate = useNavigate()
     return (
-        <div id="col">
-            <img src={"https://image.tmdb.org/t/p/w300" + movie.poster_path} className="movie-poster" alt="logo" />
-            <div className="movie-title">
+        <div id="movie-affiche" 
+        onClick={() => navigate("/details/" + movie.id)}>
+            <img src={"https://image.tmdb.org/t/p/w300" + movie.poster_path} className="movie-poster" alt="logo" title={movie.overview}
+            />
+            <div className="movie-title" >
                 {movie.title}
             </div>
             <div className="movie-date">
