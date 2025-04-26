b9c05eb586d5b7b8849e0b4f4d2748de185b00af
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-04-26 14:18:59 +0200

    (feature) adding a new header interface (with a future account/connection ?)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,10 +4,31 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="Header-container">
-      <Link className="Link" to="/">
-        Accueil
-      </Link>
-    </div>
+      <div className="left">
+        <Link className="Link" to="/">
+          {/* I put some random image from internet but I will have to change it in order to have user rights good */}
+          <img
+            className="header-icon"
+            height="30px"
+            width="30px"
+            src="https://openclipart.org/image/2400px/svg_to_png/217511/142Accueil9747035.png">
+          </img>
+        </Link>
+      </div>
+      <div className="right">
+        <Link className="Link" to="/connection">
+          Connexion
+        </Link>
+        <Link className="Link" to="/profil">
+          <img
+            className="header-icon"
+            height="30px"
+            width="30px"
+            src="https://getdrawings.com/vectors/vector-circle-png-30.png">
+          </img>
+        </Link>
+      </div>
+    </div >
   );
 };
 
