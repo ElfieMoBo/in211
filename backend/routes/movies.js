60b7ff676a9b107de1d45c33c6bcd15e0e92c34e
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-05-10 15:01:19 +0200

    (database) script to populate movie database

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -64,6 +64,36 @@ router.post('/new-movie', function (req, res) {
         })
 });
 
+router.post('/new-list-movies', function (req, res) {
+    const movieRepository = appDataSource.getRepository(Movie);
+    // Creating a new movie based on the request
+    for (let i = 0; i < 20; i++) {
+        const newMovie = movieRepository.create({
+            title: req.body.results[i].title,
+            release_date: req.body.results[i].release_date,
+            overview: req.body.results[i].overview,
+            runtime: req.body.results[i].runtime,
+            limited_age: req.body.results[i].limited_age,
+            like: req.body.results[i].like,
+            poster_path: req.body.results[i].poster_path,
+            genre_id1: req.body.results[i].genre_id1,
+            genre_id2: req.body.results[i].genre_id2,
+            genre_id3: req.body.results[i].genre_id3,
+            genre_id4: req.body.results[i].genre_id4,
+        });
+        // Inserting this new movie in the BDD
+        movieRepository
+            .insert(newMovie)
+            .then(function (newDocument) {
+                // res.status(201).json(newDocument);
+            })
+            .catch(function (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Error while creating the movie' });
+            })
+    }
+});
+
 router.delete('/delete-a-movie/:movieID', function (req, res) {
     appDataSource
         .getRepository(Movie)
