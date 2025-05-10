6a3fd8517c583f76f056b5e2cc443e2ea8ab3c39
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-05-10 15:01:29 +0200

    (database) script to populate movie database

--- populate.bash
+++ populate.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# curl -X POST -H 'Content-Type: application/json' -d @movie1.json http://localhost:8080/api/movies/new-list-movies
-# curl -X POST -H 'Content-Type: application/json' -d @movie2.json http://localhost:8080/api/movies/new-list-movies
-# curl -X POST -H 'Content-Type: application/json' -d @movie3.json http://localhost:8080/api/movies/new-list-movies
-# curl -X POST -H 'Content-Type: application/json' -d @movie4.json http://localhost:8080/api/movies/new-list-movies
-# curl -X POST -H 'Content-Type: application/json' -d @movie5.json http://localhost:8080/api/movies/new-list-movies
+curl -X POST -H 'Content-Type: application/json' -d @movie1.json $(import.meta.env.VITE_BACKDEND_URL)/movies/new-list-movies
+# curl -X POST -H 'Content-Type: application/json' -d @movie2.json $(import.meta.env.VITE_BACKDEND_URL)/movies/new-list-movies
+# curl -X POST -H 'Content-Type: application/json' -d @movie3.json $(import.meta.env.VITE_BACKDEND_URL)/movies/new-list-movies
+# curl -X POST -H 'Content-Type: application/json' -d @movie4.json $(import.meta.env.VITE_BACKDEND_URL)/movies/new-list-movies
+# curl -X POST -H 'Content-Type: application/json' -d @movie5.json $(import.meta.env.VITE_BACKDEND_URL)/movies/new-list-movies
