3060826d8f1a53a6a4e7c2c9f27946cb2baee9a1
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-05-13 09:09:38 +0200

    (feature) creating users

--- frontend/src/components/Profil/Profil.jsx
+++ frontend/src/components/Profil/Profil.jsx
@@ -1,10 +1,24 @@
 import './Profil.css';
+import axios from 'axios';
+
+function Connection(user) {
+    var passwd = prompt(`Authentification (${user.pseudo}) : entrez votre mot de passe`, "Mot de passe");
+    axios
+        .get(`${import.meta.env.VITE_BACKDEND_URL}/users/login/${user.id}/${passwd}`)
+        .then(() => {
+            alert("Success");
+        })
+        .catch((error) => {
+            alert("Bad passwd")
+        });
+};
 
 function Profil({ user }) {
     return (
         <div className="profil-container" >
             <button
                 className="user-icon"
+                onClick={() => Connection(user)}
             >
                 {user.pseudo.substring(0, 1).toUpperCase()}
             </button>
