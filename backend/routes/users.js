8c180029857cec240f6172114298ca91f7f21023
ElfieMoBo <elfie.mobo.31570@gmail.com>
2025-05-18 12:57:19 +0200

    (back) having password hashed in data base

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import { appDataSource } from '../datasource.js';
 import User from '../entities/user.js';
+import { createHash } from 'node:crypto'
+
 
 const router = express.Router();
+
+async function strHash(a, b) {
+  b = b || 'SHA-1';
+  var c = new TextEncoder().encode(a),
+    d = await crypto.subtle.digest(b, c),
+    e = Array.from(new Uint8Array(d)),
+    f = e.map(function (c) {
+      return c.toString(16).padStart(2, '0');
+    }).join('');
+  return f;
+}
 
 router.get('/', function (req, res) {
   appDataSource
@@ -14,13 +27,14 @@ router.get('/', function (req, res) {
 });
 
 router.post('/new', function (req, res) {
+  const hash = createHash('sha256').update(req.body.passwd).digest('hex');
   const userRepository = appDataSource.getRepository(User);
   const newUser = userRepository.create({
     email: req.body.email,
     lastname: req.body.lastname,
     firstname: req.body.firstname,
     pseudo: req.body.pseudo,
-    shadow: req.body.passwd,
+    shadow: hash,
     age: req.body.age,
   });
 
@@ -41,8 +55,20 @@ router.post('/new', function (req, res) {
     });
 });
 
+router.get('/user-by-pseudo/:pseudo', function (req, res) {
+  appDataSource
+    .getRepository(User)
+    .find({
+      where: { pseudo: req.params.pseudo }
+    })
+    .then(function (user) {
+      res.json({ user: user });
+    });
+});
+
 router.post('/login', function (req, res) {
   console.log(req.body)
+  const hash = createHash('sha256').update(req.body.passwd).digest('hex');
   appDataSource
     .getRepository(User)
     .find({
@@ -52,7 +78,7 @@ router.post('/login', function (req, res) {
     })
     .then(function (user) {
       console.log("user", user)
-      user[0].shadow == req.body.passwd ? (
+      user[0].shadow == hash ? (
         res.status(201).json(user)
       ) : res.status(400).json({
         message: `Mot de passe incorrect pour (${user[0].pseudo})`,
@@ -62,17 +88,17 @@ router.post('/login', function (req, res) {
       console.error(error);
       res.status(500).json({ message: "Ce nom d'utilisateur est inconnu" });
     });
-})
+});
 
-router.delete('/:userId', function (req, res) {
+router.delete('/delete/:userId', function (req, res) {
   appDataSource
     .getRepository(User)
     .delete({ id: req.params.userId })
     .then(function () {
-      res.status(204).json({ message: 'User successfully deleted' });
+      res.status(204).json({ message: "L'utilisateur a été supprimé avec succès" });
     })
     .catch(function () {
-      res.status(500).json({ message: 'Error while deleting the user' });
+      res.status(500).json({ message: "Une erreur est survenue lors de la suppression de l'utilisateur" });
     });
 });
 
